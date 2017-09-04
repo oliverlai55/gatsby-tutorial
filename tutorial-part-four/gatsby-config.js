61c51d683a549d2cf214d370ba52581e05e22ca0
oliverlai55 <oliverlai55@gmail.com>
2017-09-04 17:04:50 -0400

    Continue with tutorial

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: "Example Title"
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`
-      }
-    },
-    `gatsby-plugin-glamor`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    }
-  ]
+	siteMetadata: {
+		title: 'Example Title'
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `src`,
+				path: `${__dirname}/src/`
+			}
+		},
+		`gatsby-transformer-remark`,
+		`gatsby-plugin-glamor`,
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography`
+			}
+		}
+	]
 };
